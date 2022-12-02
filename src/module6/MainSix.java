cb5e9e9f7d2fa4b8e76cb72fd13f5b15cc18315b
Kushagra <schixor88@gmail.com>
2022-12-03 00:15:18 +0545

    m6

---
+++ src/module6/MainSix.java
@@ -0,0 +1,18 @@
+package module6;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainSix {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setSize(400,400);
+        frame.setTitle("Module 6");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        frame.getContentPane().setBackground(new Color(100,200,200));
+    }
+}

42e11d2d4dc293107e889a02b7ef16144cae1707
Kushagra <schixor88@gmail.com>
2022-12-13 19:21:16 +0545

    m12

---
+++ src/module12/MainTwelve.java
@@ -0,0 +1,33 @@
+package module12;
+
+import javax.swing.*;
+
+public class MainTwelve {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setTitle("Twelve");
+        frame.setVisible(true);
+
+        frame.setLayout(null);
+
+        JLabel label1 = new JLabel("Label 1");
+        label1.setBounds(20,20,100,20);
+        frame.add(label1);
+
+        JLabel label2 = new JLabel("Label 2");
+        label2.setBounds(150,200,100,20);
+        frame.add(label2);
+
+        JButton button1 = new JButton("Button 1");
+        button1.setBounds(200,300,100,30);
+        frame.add(button1);
+
+        JButton button2 = new JButton("Button 2");
+        button2.setBounds(20,70,100,30);
+        frame.add(button2);
+
+
+    }
+}

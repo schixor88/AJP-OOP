33ee993e3166711548e557e89e306245462f345a
Kushagra <schixor88@gmail.com>
2022-12-13 19:22:33 +0545

    m14

---
+++ src/module14/ModuleFourteen.java
@@ -0,0 +1,42 @@
+package module14;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ModuleFourteen {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setSize(400,400);
+        frame.setTitle("Fourteen");
+
+
+
+        frame.setLayout(new GridLayout(2,2));
+
+        JButton one = new JButton("1");
+        JButton two = new JButton("2");
+        JButton three = new JButton("3");
+        JButton four = new JButton("4");
+        JButton five = new JButton("5");
+        JButton six = new JButton("6");
+        JButton seven = new JButton("7");
+        JButton eight = new JButton("8");
+        JButton nine = new JButton("9");
+        JButton zero = new JButton("0");
+
+        frame.add(one);
+        frame.add(two);
+        frame.add(three);
+        frame.add(four);
+        frame.add(five);
+        frame.add(six);
+        frame.add(seven);
+        frame.add(eight);
+        frame.add(nine);
+        frame.add(zero);
+
+        frame.setVisible(true);
+
+    }
+}
